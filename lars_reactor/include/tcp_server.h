3ce7359d31599e5f79ed4150358c2f7deea4ec22
impact-eintr <yixingwei4@gmail.com>
2021-09-23 21:02:04 +0800

    准备测试

--- lars_reactor/include/tcp_server.h
+++ lars_reactor/include/tcp_server.h
@@ -1,2 +1,21 @@
 #pragma once
-void lars_hello();
+#include <cstdint>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+class tcp_server {
+public:
+  // server的构造函数
+  tcp_server(const char *ip, uint16_t port);
+
+  // 开始提供创建链接服务
+  void do_accept();
+
+  // 链接对象的析构
+  ~tcp_server();
+
+private:
+  int _sockfd;                  // 套接字
+  struct sockaddr_in _connaddr; // 客户端链接地址
+  socklen_t _addrlen;           // 客户端链接地址长度
+};
